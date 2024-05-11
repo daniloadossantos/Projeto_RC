2465fb1f3b6757b96877285642bd6fb26e7ef455
Victor Ercilio Akeno Yamane <Victor.Ercilio@hotmail.com>
2024-05-11 09:24:02 -0300

    add hasCep db

--- RC Web/php/db/db.php
+++ RC Web/php/db/db.php
@@ -163,6 +163,55 @@ class DB
 			$conectar = null;
 		}
 	}
+
+	public static function hasCEP(string $cep): bool
+	{
+		try
+		{
+			require(DB::$path);
+			$res = $conectar->prepare("SELECT * FROM $db.".DB::$cep." WHERE cod = '".$cep."'");
+			$res->execute();
+			$ceps = 0;
+			while($reg = $res->fetch(PDO::FETCH_ASSOC))
+				$ceps++;
+			return ($ceps != 0) ? true : false;
+		}
+		catch(PDOException $e)
+		{
+			echo "Erro em  hasCEPs. ".$e->getMessage();
+		}
+		catch(Exception $e)
+		{
+			echo "Erro: ".$e->getMessage();
+		}
+		finally
+		{
+			$conectar = null;
+		}
+	}
+
+	public static function fetchCEP(string $cep): CEP
+	{
+		try
+		{
+			$url = "https://viacep.com.br/ws/$cep/json/";
+			$res = file_get_contents($url);
+			$res = json_decode($res);
+			$cep = new CEP(
+				$res->cep,
+				$res->uf,
+				$res->localidade,
+				$res->bairro,
+				$res->logradouro,
+				$res->logradouro
+			)
+			return $cep;
+		}
+		catch(Exception $e)
+		{
+			echo "Erro em fetch CEPs. ".$e->getMessage();
+		}
+	}
 	
 	public static function delCEP(string $cod)
 	{
