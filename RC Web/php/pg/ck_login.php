b75ef21c2ca45fd3f815d2b2c1de3854e08e0800
Victor Ercilio Akeno Yamane <victor.ercilio@hotmail.com>
2024-05-13 00:29:44 -0300

    uncomment

--- RC Web/php/pg/ck_login.php
+++ RC Web/php/pg/ck_login.php
@@ -16,7 +16,7 @@ if( isset($_POST['nome']) && isset($_POST['senha']))
         ];
         var_dump($_SESSION);
         // Por hora fica com .html
-        // header('Location: ./../../agendamento.html');
+        header('Location: ./../../agendamento.html');
     }
     else if($_POST['nome'] === 'adm' && $_POST['senha'] === 'adm')
     {
