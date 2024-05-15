52cc666eb9c7a392437e29a32b02a1d575800d75
Victor Ercilio Akeno Yamane <victor.ercilio@hotmail.com>
2024-05-14 22:29:03 -0300

    start crud solicitacao

--- RC Web/php/pg/crud_age.php
+++ RC Web/php/pg/crud_age.php
@@ -6,16 +6,40 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '/Projeto_RC/RC Web/php/cls/cliente.php
 require_once($_SERVER['DOCUMENT_ROOT'] . '/Projeto_RC/RC Web/php/cls/solicitacoes.php');
 require_once($_SERVER['DOCUMENT_ROOT'] . '/Projeto_RC/RC Web/php/cls/tecnico.php');
 require_once($_SERVER['DOCUMENT_ROOT'] . '/Projeto_RC/RC Web/php/cls/cep.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/Projeto_RC/RC Web/php/util/log.php');
 
-if( !isset($_SESSION['USER']) )
-{
-header('Location: ./index.php');
-}
+setExib(true);
 
 $cli = DB::getClientes();
-$ceps = DB::getCEPs();
 $tec = DB::getTecnicos();
 $sol = DB::getSolicitacoes();
+
+var_dump($_POST);
+
+function makeSolicitacao()
+{   
+    $data_age = "";
+    if(isset($_POST['dia']) && 
+        isset($_POST['mes']) &&
+        isset($_POST['dia']) &&
+        isset($_POST['hoa'])
+    )
+    {
+        $data_age = $_POST['ano'] . $_POST['mes'] . $_POST['dia'] . " " . $_POST['hora'] . ":00:00";
+    }
+    $solicitacao = new Solicitacao(
+        $_POST['cod'],
+        $_POST['codCli'],
+        isset($_POST['codTec'])?$_POST['codTec']:0,
+        isset($_POST['codServ'])? $_POST['codServ']: 0,
+        isset($_POST['codAte'])?$_POST['cod']:0,
+        isset($_POST['codOrc'])?$_POST['cod']:0,
+        ($data_age != "")?$data_age:"",
+        "",
+        "",
+        isset($_POST['status'])?$_POST['status']:"",
+    );
+}
 
 $CREATE = 'CREATE';
 $READ = 'READ';
@@ -26,13 +50,9 @@ if(isset($_POST[$CREATE]))
 {
     try
     {
-        $nome = empty($_POST['nome_pf']) ? $_POST['nome_pj'] : $_POST['nome_pf'];
-        echo $nome;
-        checkCEP();
-        $cliente = makeCliente();
-        $cliente->show();
-        DB::insertCliente($cliente);
-        exib("Cliente criado");
+        $solicitacao = makeSolicitacao();
+        DB::insertSolicitacao($solicitacao);
+        exib("Solicitacao criada");
     }
     catch(Exception $e)
     {
@@ -45,12 +65,12 @@ else if(isset($_POST[$DELETE]))
     try
     {
         $cod = (int) $_POST['cod'];
-        DB::delCliente($cod);
-        exib("Cliente deletado");
+        DB::deleteSolicitacao($cod);
+        exib("Solicitacao deletado");
     }
     catch(Exception $e)
     {
-        echo "Erro no $READ cliente" . $e->getMessage();
+        echo "Erro no $DELETE cliente" . $e->getMessage();
     }
 }
 else if(isset($_POST[$UPDATE]))
@@ -59,21 +79,21 @@ else if(isset($_POST[$UPDATE]))
     try
     {
         $cod = $_POST['cod'];
-        checkCEP();
-        $cliente = makeCliente();
-        DB::updateCliente($cliente);
-        exib("Cliente atualizado");
+        $solicitacao = makeSolicitacao();
+        DB::updateSolicitacao($solicitacao);
+        exib("Solicitacao atualizada");
     }
     catch(Exception $e)
     {
-        echo "Erro no $READ cliente" . $e->getMessage();
+        echo "Erro no $UPDATE cliente" . $e->getMessage();
     }
 }
 else if(isset($_POST[$READ]))
 {
     echo "Vou procurar";
 }
-exib("<button><a href=\"../../agendamento.php\">Clientes</a></button>");
+
+exib("<button><a href=\"../../agendamento.php\">Solicitacao</a></button>");
 
 setExib(false);
 // header('Location: ./../../agendamento.php');
